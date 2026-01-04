025a380cac3dd2d75fe75e09689d9d6863d24a56
Meylis Malikov <malikmeylis@gmail.com>
2026-01-04 21:29:02 +0100

    Fix: Add project files and ignore terraform binaries

---
+++ Data Modeling, Transformation and Serving/Capstone Project Part 2 - Data Qual and Orch  and Vis/terraform/backend.tf
@@ -0,0 +1,5 @@
+terraform {
+  backend "local" {
+    path = "/home/coder/.local/share/code-server/User/de-c4w4a2-909851488623-us-east-1-terraform-state.state"
+  }
+}
---
+++ Data Storage and Queries/Building a Data Lakehouse with AWS Lake Formation and Apache Iceberg/terraform/backend.tf
@@ -0,0 +1,5 @@
+terraform {
+  backend "local" {
+    path = "/home/coder/.local/share/code-server/User/de-c3w2a1-434822248603-us-east-1-terraform-state.state"
+  }
+}
